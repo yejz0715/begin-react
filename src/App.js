638d950673d6ee51c4f355bb125475f5edb12e9b
unknown <yejz0715@gmail.com>
2022-12-16 17:25:47 +0900

    main 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Name from "./components/Name";
+import Gender from "./components/Gender";
+import Age from "./components/Age";
+import Email from "./components/Email";
+import Study from "./components/Study";
+import img from "./assets/img.png";
 
+import "./style/style.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        <img src={img} alt="img" width="100px" />
+        예지를 소개합니다.
+      </h1>
+      <div id="wrapper">
+        <Name />
+        <Gender />
+        <Age />
+        <Email email="yeze0715@gmail.com" />
+        <Study study="react" />
+      </div>
     </div>
   );
 }
