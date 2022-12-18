a8b2b1a4690674f1f1d6f147f1cb957c24d72a6d
unknown <yejz0715@gmail.com>
2022-12-18 13:46:52 +0900

    Link 추가

---
+++ src/pages/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const List = () => {
+  return (
+    <div>
+      <h1>List Page</h1>
+      <Link to="/profile">Profile</Link>
+    </div>
+  );
+};
+export default List;
